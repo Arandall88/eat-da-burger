37e986b484148964801de387e8eadc59dcbf026a
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-11 16:14:55 -0500

    fixed errors. changed create burger to insertOne, fixed functionality! yeee!

--- models/burger.js
+++ models/burger.js
@@ -8,6 +8,13 @@ selectAll: function(cb) {
         cb(res);
     });
 },
+
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burger", cols, vals, function(res) {
+      cb(res);
+    });
+  },
      updateOne: function(objColVals, condition, cb) {
     orm.updateOne("burger", objColVals, condition, function(res) {
       cb(res);
