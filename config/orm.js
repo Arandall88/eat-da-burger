07f740863a8c3742d4580ab56ebefcdc35afed10
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-10 21:48:41 -0500

    delete burget from the db

--- config/orm.js
+++ config/orm.js
@@ -88,58 +88,22 @@ var orm = {
     });
 },
   
-  create: function (table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function (table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete: function (table, condition, cb) {
+  // Delete a burger from the db.
+  deleteOne: function(table, condition, cb) {
     var queryString = "DELETE FROM " + table;
     queryString += " WHERE ";
     queryString += condition;
 
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
+    console.log(queryString);
 
-      cb(result);
+    connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err
+        }
+        cb(result);
     });
-  },
+}
 };
 
-// Export the orm object for the model (cat.js).
+// Export the ORM object in module.exports.
 module.exports = orm;
