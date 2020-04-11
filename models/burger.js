0cb8d20a4d30cfb2fda8659ff7fe61959bde4d77
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-10 22:14:14 -0500

    added update select and delete functions

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,22 @@
 var orm = require("../config/orm.js");
+
+var burger = {
+selectAll: function(cb) {
+    orm.selectAll("burgers", function(res){
+        cb(res);
+    });
+},
+updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+
+};
+module.exports = burger;
