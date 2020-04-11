2da80c7c1e5cbf53c5d5963ee5701aaa77cc63b8
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-10 21:32:43 -0500

    add burger to the db

--- config/orm.js
+++ config/orm.js
@@ -51,6 +51,25 @@ var orm = {
       cb(result);
     });
   },
+    // Add a burger to the db.
+    insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+
+      console.log(queryString);
+
+      connection.query(queryString, vals, function(err, result) {
+          if (err) {
+              throw err
+          }
+          cb(result);
+      });
+  },
   create: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
